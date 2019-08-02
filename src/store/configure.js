e8afd6e18505d04aacd60c43abe7ac8fd99986d1
CallbackWJ <>
2019-08-02 11:25:40 +0900

    store의 액션이름 수정 [init]==>[INIT]

--- src/store/configure.js
+++ src/store/configure.js
@@ -1,6 +1,6 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers } from "redux";
 import * as modules from "./modules";
 
 const configure = () => createStore(combineReducers(modules));
- 
+
 export default configure;
--- src/store/modules/baggage.js
+++ src/store/modules/baggage.js
@@ -1,34 +1,37 @@
-import {createAction, handleActions} from 'redux-actions';
+import { createAction, handleActions } from "redux-actions";
 // action types
 
-const INIT = 'baggage/INIT';
-const SET_LINE_LENGTH='baggage/SET_LINE_LENGTH';
-const SET_MAX_WEIGHT='baggage/SET_MAX_WEIGHT';
-const SET_BAGGAGE_LIST='baggage/SET_BAGGAGE_LIST';
+const INIT = "baggage/INIT";
+const SET_LINE_LENGTH = "baggage/SET_LINE_LENGTH";
+const SET_MAX_WEIGHT = "baggage/SET_MAX_WEIGHT";
+const SET_BAGGAGE_LIST = "baggage/SET_BAGGAGE_LIST";
 // action creators
 
 export const init = createAction(INIT);
-export const setLineLength=createAction(SET_LINE_LENGTH);
-export const setMaxWeight=createAction(SET_MAX_WEIGHT);
-export const setBaggageList=createAction(SET_BAGGAGE_LIST);
+export const setLineLength = createAction(SET_LINE_LENGTH);
+export const setMaxWeight = createAction(SET_MAX_WEIGHT);
+export const setBaggageList = createAction(SET_BAGGAGE_LIST);
 
 // initial state
 const initialState = {
-    lineLength: 0,
-    maxWeight: 0,
-    baggageList: [],
+  lineLength: 0,
+  maxWeight: 0,
+  baggageList: []
 };
 
 // reducer
-export default handleActions({
-    [init]: (state, action) => initialState,
+export default handleActions(
+  {
+    [INIT]: (state, action) => initialState,
     [SET_LINE_LENGTH]: (state, action) => {
-        return { ...state,lineLength: action.payload };
+      return { ...state, lineLength: action.payload };
     },
     [SET_MAX_WEIGHT]: (state, action) => {
-        return {  ...state,maxWeight: action.payload };
+      return { ...state, maxWeight: action.payload };
     },
     [SET_BAGGAGE_LIST]: (state, action) => {
-        return {  ...state,baggageList: action.payload };
-    },
-}, initialState);
+      return { ...state, baggageList: action.payload };
+    }
+  },
+  initialState
+);
